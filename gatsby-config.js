ee101a2be22c88802ab724dac708497816a2d05b
Ric Smolenski <ricsmo@ricsmo.com>
2023-02-23 16:40:38 -0700

    second commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,27 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [],
+  siteMetadata: {
+    title: `Your Site Title`,
+    description: `A description for your blazing fast site, using multiple themes!`,
+    author: `Your name`,
+    social: [
+      {
+        name: `Twitter`,
+        url: `https://twitter.com/gatsbyjs`,
+      },
+      {
+        name: `GitHub`,
+        url: `https://github.com/gatsbyjs`,
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: `gatsby-theme-notes`,
+      options: {
+        basePath: `/notes`,
+      },
+    },
+  ],
 }
